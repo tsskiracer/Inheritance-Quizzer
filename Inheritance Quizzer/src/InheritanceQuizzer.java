75fe8f7710e6e03761e9d5b889dff35b9eb5d03a
tsskiracer <william.starkey@MU-C1-03.mullen.pvt.k12.co.us>
2016-01-12 09:08:01 -0700

    initial commit

---
+++ Inheritance Quizzer/src/InheritanceQuizzer.java
@@ -0,0 +1,24 @@
+
+public class InheritanceQuizzer
+	{
+
+		public static void main(String[] args)
+			{
+				System.out.println("Hello and welcome to the inheritance quizzer");
+				System.out.println();
+				System.out.println();
+				System.out.println("The code below will be referenced for this quiz:");
+				System.out.println("public class Vehicle");
+				System.out.println("     { ");
+				System.out.println("     protected String brand;");
+				System.out.println("     public Vehicle()");
+				System.out.println("            {*some code here*}");
+				System.out.println("     public numWheels(int quantity)");
+				System.out.println("            {*some code here*}");
+				System.out.println("     ");
+				
+		        
+				
+			}
+
+	}
