ed6536ccbc8b90e6e0ddbb5d71c9b1a9c791d285
tsskiracer <William.Starkey@MU-C1-03.mullen.pvt.k12.co.us>
2016-01-13 09:11:12 -0700

    modified

--- Inheritance Quizzer/src/InheritanceQuizzer.java
+++ Inheritance Quizzer/src/InheritanceQuizzer.java
@@ -8,17 +8,44 @@ public class InheritanceQuizzer
 				System.out.println();
 				System.out.println();
 				System.out.println("The code below will be referenced for this quiz:");
-				System.out.println("public class Vehicle");
-				System.out.println("     { ");
-				System.out.println("     protected String brand;");
-				System.out.println("     public Vehicle()");
-				System.out.println("            {*some code here*}");
-				System.out.println("     public numWheels(int quantity)");
-				System.out.println("            {*some code here*}");
-				System.out.println("     ");
+				printCode();
 				
 		        
 				
 			}
-
+		public static void printCode()
+		{
+			System.out.println("public class Vehicle");
+			System.out.println("     { ");
+			System.out.println("     protected String brand;");
+			System.out.println("     public Vehicle()");
+			System.out.println("            {*some code here*}");
+			System.out.println("     public Vehicle(int numWheels)");
+			System.out.println("            {*some code here*}");
+			System.out.println("     public boolean adrenalineRush()");
+			System.out.println("            {*some code here*}");
+			System.out.println("     }");
+			System.out.println();
+			System.out.println();
+			System.out.println("public class Car extends Vehicle     ");
+			System.out.println("     {");
+			System.out.println("     public Car()");
+			System.out.println("            {*some code here*}");
+			System.out.println("     public boolean racecar()");
+			System.out.println("            {*some code here*}");
+			System.out.println("     }");
+			System.out.println();
+			System.out.println();
+			System.out.println("     public class Truck extends Vehicle");
+			System.out.println("     {");
+			System.out.println("     public Truck()");
+			System.out.println("            {*some code here*}");
+			System.out.println("     public Truck(int loadCapacity) ");
+			System.out.println("            {*some code here*}");
+			System.out.println("     public boolean sleeperCab()");
+			System.out.println("            {*some code here*}");
+			System.out.println("     }");
+			
+			
+		}
 	}
